2f636f1e6ed32412b3248254451dccbff63543b8
robert.lo <atttrobert@gmail.com>
2017-07-13 00:27:07 +0800

    [import] - TextView and Button exercises

--- Thefirstproject/app/src/main/java/com/example/mac/thefirstproject/MainActivity.java
+++ Thefirstproject/app/src/main/java/com/example/mac/thefirstproject/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.mac.thefirstproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+        Log.i("Info","Button is being pressed !!");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
