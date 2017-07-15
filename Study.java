f1ed966a28cfbd4b49a5cad2fd817282ea6e1480
robert.lo <robert.lo@amtran.com.tw>
2017-07-15 16:29:08 +0800

    [Update] - To update Android study for imageView, TextView and EditView

---
+++ Study.java
@@ -0,0 +1,9 @@
+// To display message
+Toast.MakeText();
+
+// To show a image 
+// To download the image file and then resize it into a proper pixel size
+// To drag the file into the drawable folder of Android project
+
+ImageView thirdImage = (ImageView) findViewById(R.id.imageView3);
+
